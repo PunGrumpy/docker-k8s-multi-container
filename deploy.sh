c9c37608965109e362e0bb83e9ddc135ded1c52b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-04-01 18:10:23 +0700

    feat(*): added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+# Build the images
+docker build -t $DOCKER_USERNAME/multi-client:latest -t $DOCKER_USERNAME/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t $DOCKER_USERNAME/multi-server:latest -t $DOCKER_USERNAME/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t $DOCKER_USERNAME/multi-worker:latest -t $DOCKER_USERNAME/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+# Push the images to Docker Hub
+docker push $DOCKER_USERNAME/multi-client:latest && docker push $DOCKER_USERNAME/multi-client:$SHA
+docker push $DOCKER_USERNAME/multi-server:latest && docker push $DOCKER_USERNAME/multi-server:$SHA
+docker push $DOCKER_USERNAME/multi-worker:latest && docker push $DOCKER_USERNAME/multi-worker:$SHA
+
+# Kubernetes
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=$DOCKER_USERNAME/multi-client:$SHA
+kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_USERNAME/multi-worker:$SHA
